e76fe9f14a819a5f72526bb6400904dc5c35b27c
Lucas Matheus <lucasdmc40@gmail.com>
2026-07-03 16:38:51 +0000

    adicionar tarefa especifica

--- main.py
+++ main.py
@@ -1,7 +1,50 @@
 from fastapi import FastAPI
+from datetime import datetime
 
+LISTA_TAREFAS = []
 APP = FastAPI()
+
+def nova_tarefa(id: int, titulo: str, descricao: str):
+    """Função auxiliar para criar uma tarefa usando dicionário (`dict`)"""
+    return {
+        "id": id,
+        "titulo": titulo,
+        "descricao": descricao,
+        "concluido": False,
+        "criado_em": datetime.now()
+    }
 
 @APP.get("/")
 def index():
-    return "Olá, DevOps"
+    return "Olá, DevOps!"
+
+@APP.get("/tarefas")
+def listar_tarefas():
+    global LISTA_TAREFAS
+
+    # Lista tarefas (somente id e titulo)
+    if len(LISTA_TAREFAS) == 0:
+        return LISTA_TAREFAS
+
+    tarefas = []
+    
+    for tarefa in LISTA_TAREFAS:
+        info = {"id": tarefa['id'], "titulo": tarefa['titulo']}
+        tarefas.append(info)
+
+    return tarefas
+
+@APP.get("/tarefas/{id}")
+def listar_tarefa_especifica(id: int):
+    global LISTA_TAREFAS
+    LISTA_TAREFAS.append(nova_tarefa(0, "nova tarefa", "descricao nova tarefa"))
+
+    mensagem_padrao = {"mensagem": "Não existe nenhuma tarefa"}
+    if len(LISTA_TAREFAS) == 0:
+        return mensagem_padrao
+    
+    # ID da tarefa é o índice na lista
+    if id >= 0 and id < len(LISTA_TAREFAS):
+        return LISTA_TAREFAS[id]
+    
+    return mensagem_padrao
